fc8eb50e2d3a1ffeef2c80a66297eda7f661e108
Cheol-Will <kangcs7083@naver.com>
2025-02-24 09:44:56 +0900

    modify structure

--- StochasticDepth.py
+++ StochasticDepth.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torchsummary import summary
 
 class StDepthBlock(nn.Module):
-   
+
     def __init__(self, in_channels, out_channels, stride,  survival_probabily, padding):
         super(StDepthBlock, self).__init__()
         self.survival_probabily = survival_probabily
@@ -33,18 +33,16 @@ class StDepthBlock(nn.Module):
         out = self.shortcut(x)
         if self.channel_pad > 0:
             out = F.pad(out, (0, 0, 0, 0, self.channel_pad, self.channel_pad))
-       
         if torch.bernoulli(self.survival_probabily) == 1:
-            print(f"shape of out: {out.shape}")
-            print(f"self.channel_pad: {self.channel_pad}")
-            print(f"in_channel: {self.in_channels}, out_channel: {self.out_channels}")
+            a = self.res(x)
+            
             out += self.res(x)
 
         return F.relu(out)
-    
+
 class StDepth(nn.Module):
     """
-        3 groups with 18 residual blocks each. 
+        3 groups with 18 residual blocks each.
         # of filters in each group are 16, 32, 64
     """
 
@@ -52,18 +50,18 @@ class StDepth(nn.Module):
         super(StDepth, self).__init__()
         self.in_channels = 16
 
-        self.conv1 = nn.Conv2d(3, 16, 7, stride = 2)
+        self.conv1 = nn.Conv2d(3, 16, 7, stride = 1, padding = 3)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu1 = nn.ReLU()
         self.st1 = self.make_layer(block, 16, num_blocks_list[0], 1, False)
         self.st2 = self.make_layer(block, 32, num_blocks_list[1], 2, True)
         self.st3 = self.make_layer(block, 64, num_blocks_list[2], 2, True)
-        
+
         self.clf = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
+            nn.AdaptiveAvgPool2d(1),  
             nn.Flatten(),
-            nn.Linear(512 * 1 * 1, 1000),
-            nn.LogSoftmax(dim = 1) 
+            nn.Linear(64 * 1 * 1, 1000),
+            nn.LogSoftmax(dim = 1)
         )
 
     def make_layer(self, block, out_channels, num_blocks, first_stride, padding):
@@ -73,7 +71,7 @@ class StDepth(nn.Module):
         L = len(stride_list)
         for idx, stride in enumerate(stride_list):
             p = torch.tensor(1 - 0.5/L * idx)
-            layers.append(block(self.in_channels, out_channels, stride, p, padding))            
+            layers.append(block(self.in_channels, out_channels, stride, p, padding))
             self.in_channels = out_channels
 
         # weight initialization kaiming normal
@@ -92,11 +90,13 @@ class StDepth(nn.Module):
         x = self.st1(x)
         x = self.st2(x)
         x = self.st3(x)
-        
+        print(f"shape before clf: {x.shape}")
         x = self.clf(x)
         return x
-    
-# model = StDepth(StDepthBlock, [6, 6, 6])
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# model.to(device) 
-# summary(model, input_size=(3, 32, 32), device=str(device))
+
+
+
+model = StDepth(StDepthBlock, [6, 6, 6])
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+summary(model, input_size=(3, 256, 256), device=str(device))
