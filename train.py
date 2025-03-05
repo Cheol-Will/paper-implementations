deef021455958fbaa6e8c731eb1b819cda2ba013
Cheol-Will <kangcs7083@naver.com>
2025-03-05 16:41:35 +0900

    add tensorboard

--- train.py
+++ train.py
@@ -1,11 +1,10 @@
-import argparse, os, time
+import argparse, os
 
 import torch
 import torchvision
 import torchvision.transforms as transforms
 from torchvision.transforms import v2
 from torch.utils.tensorboard import SummaryWriter
-from torchsummary import summary
 
 from ResNet import ResNet, ResBlock
 from PreActResNet import PreActResNet, PreActResBlock
@@ -15,14 +14,14 @@ from ConvMixer import ConvMixer, ConvMixerBlock
 
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime
 
 torch.manual_seed(42)
 CUR_DIR = os.getcwd()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--output_dir", default = os.path.join(CUR_DIR, "output"))
-parser.add_argument("--model", default="ResNet", choices=["ResNet", "PreActResNet", "DenseNet", "StDepth"])
+parser.add_argument("--model", default="ResNet", 
+    choices=["ResNet", "PreActResNet", "DenseNet", "StDepth", "ConvMixer", "MLPMixer"])
 parser.add_argument("--batch_size", default=64, type=int)
 
 def imshow(img, filename="output.png"):
@@ -77,30 +76,39 @@ def test_loop(dataloader, model, loss_fn, device):
 
     return correct, test_loss
 
-def train_epochs(args, model, trainloader, testloader, loss_fn, device, optimizer, EPOCHS):
-    best_tloss = 1_000_000
-    start = time.time()
-
+def train_epochs(args, model, trainloader, testloader, loss_fn, device, optimizer, scheduler, EPOCHS):
+    min_test_loss = 1_000_000
+    writer = SummaryWriter(f'runs/models')
     for epoch in range(EPOCHS):
         print(f"Epoch {epoch+1}\n-------------------------------")
 
         model.train(True)
         avg_loss = train_loop(trainloader, model, loss_fn, optimizer, device, args.batch_size)
+
         model.eval()
         correct, test_loss = test_loop(testloader, model, loss_fn, device)
+
         print(f'Train Error: {avg_loss:>7f}')
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-        # Track best performance, and save the model's state
-        if test_loss < best_tloss and epoch > 30:
-            best_tloss = test_loss
-            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-            model_path = os.path.join(CUR_DIR, f"output/{args.model}_weights_{timestamp}_{epoch}.pth")
+        # Track best performance, and save the model
+        if test_loss < min_test_loss and epoch > 30:
+            min_test_loss = test_loss
+
+            model_path = os.path.join(args.output_dir, f"{args.model}_weights_best_model_{epoch}.pth")
             print(f"save model in {model_path}")
             torch.save(model.state_dict(), model_path)
 
-    end = time.time()
-    print(f"{end - start:.5f} sec")
+        # write Accuracy and Loss
+        n_iter = epoch * len(trainloader)
+        writer.add_scalar(f"Acc/{args.model}", correct, n_iter)
+        writer.add_scalar(f"Loss/{args.model}", test_loss, n_iter)
+
+        scheduler.step()
+
+
+    writer.flush()
+    writer.close()
 
 def build_loader(args):
     transform = transforms.Compose(
@@ -124,22 +132,23 @@ def build_loader(args):
     return trainloader, testloader
 
 def main(args):
+    if not os.path.isdir(args.output_dir):
+        print(f'Output directory {args.output_dir} does not exist; creating it')
 
-    # writer = SummaryWriter(f'runs/cifar-10-{parser.model}')
     trainloader, testloader = build_loader(args)
-
+        
     if args.model == "ResNet":         
-        model = ResNet(ResBlock, num_blocks_list = [18, 18, 18], dataset = "cifar-10")
+        model = ResNet(ResBlock, channel_list = [16, 32, 64], num_blocks_list = [18, 18, 18], dataset = "cifar-10")
     elif args.model == "PreActResNet":
-        model = PreActResNet(PreActResBlock, num_blocks_list = [18, 18, 18], dataset = "cifar-10")
+        model = PreActResNet(PreActResBlock, channel_list = [16, 32, 64], num_blocks_list = [18, 18, 18], dataset = "cifar-10")
     elif args.model == "DenseNet":
         model = DenseNet(DenseBlock, num_blocks_list = [13, 13, 13], growth_rate = 12, in_channels = 16)
     elif args.model == "StDepth":
-        model = StDepth(StDepthBlock, [18, 18, 18])
+        model = StDepth(StDepthBlock, channel_list = [16, 32, 64], num_blocks_list = [18, 18, 18])
     elif args.model == "ConvMixer":
         model = ConvMixer(h=256, d=8, p=1, k=15, num_class=10)
     elif args.model == "MLPMixer":
-        model = MLPMixer(128, 4, 256, 64, 32, 32, 10)
+        model = MLPMixer(c = 128, p = 4, c_hidden = 256, s_hidden = 64, h = 32, w = 32, num_class = 10)
     else: 
         print("Check if the model name is correct")
         exit()
@@ -152,9 +161,8 @@ def main(args):
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 225], gamma=0.1)
 
     epochs = 300
-    train_epochs(args, model, trainloader, testloader, loss_fn, device, optimizer, epochs)
-
-    torch.save(model.state_dict(), os.path.join(CUR_DIR, f"output/{args.model}_weights.pth"))
+    train_epochs(args, model, trainloader, testloader, loss_fn, device, optimizer, scheduler, epochs)
+    torch.save(model.state_dict(), os.path.join(args.output_dir, f"output/{args.model}_weights.pth"))
 
     return 
 
