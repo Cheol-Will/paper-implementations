660846155c98133574107860bd0811d77902379a
Cheol-Will <kangcs7083@naver.com>
2025-04-03 00:51:26 +0900

    implemented multihead self attention

--- models/vision_transformer.py
+++ models/vision_transformer.py
@@ -2,13 +2,64 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torchsummary import summary
+class SelfAttention(nn.Module):
+    def __init__(self, hidden_dim, num_heads):
+        super(SelfAttention, self).__init__()
+        self.query = nn.Linear(hidden_dim, hidden_dim)
+        self.key = nn.Linear(hidden_dim, hidden_dim)
+        self.value = nn.Linear(hidden_dim, hidden_dim)
+        self.proj = nn.Linear(hidden_dim, hidden_dim)
+
+    def forward(self, query, key, value):
+        Q = self.query(query)
+        K = self.key(key).transpose(1, 2) # (batch, seq, dim) -> (batch, dim, seq)
+        V = self.value(value)
+
+        # softmax along seq dimension and normalize
+        # K Transpose 반영 필요
+        A = F.softmax(torch.einsum("bij,bjk->bik", Q, K), dim=1) / (hidden_dim**(1/2))
+        V = torch.einsum("bij,bjk->bik", A, V)
+        out = self.proj(V)
+        return out, A
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, hidden_dim, num_heads, batch_first=True):
+        super(MultiheadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.head_dim = hidden_dim // num_heads 
+        self._query = nn.Linear(hidden_dim, hidden_dim)
+        self._key = nn.Linear(hidden_dim, hidden_dim)
+        self._value = nn.Linear(hidden_dim, hidden_dim)  
+        self.proj = nn.Linear(hidden_dim, hidden_dim)
+
+    def forward(self, query, key, value):
+        # input shape: (batch_size, seq_length, hidden_dim)
+        # X: (B, N, D)
+        # Q, K, V: (B, N, D) -> (B, N, K, D/K)
+
+        B, N, D = query.shape
+        H = self.num_heads
+        head_dim = self.head_dim
+
+        # (B, N, D) -> (B, N, H, D') -> (B, H, N, D')
+        Q = self._query(query).reshape(B, N, H, head_dim).transpose(1,2)
+        K = self._key(key).reshape(B, N, H, head_dim).transpose(1,2)
+        V = self._value(value).reshape(B, N, H, head_dim).transpose(1,2)
+
+        # (batch, head, seq, dim)
+        A = F.softmax(torch.einsum("bhij,bhkj->bhik", Q, K) / (head_dim**(0.5)), dim=-1) # (B, H, N, N)
+        V_ = torch.einsum("bhij,bhjk->bhik", A, V) # (B, H, N, D')
+        V_ = V_.transpose(1, 2).reshape(B, N, D) # (B, N, H, D') -> (B, N, D)
+        out = self.proj(V_)
+
+        return out, A
+
 
 class VisionTransformerBlock(nn.Module):
     def __init__(self, hidden_dim, num_heads, mlp_dim):
         super(VisionTransformerBlock, self).__init__()
         self.ln1 = nn.LayerNorm(hidden_dim)
-        self.multi_head_attention = nn.MultiheadAttention(hidden_dim, num_heads, batch_first=True) # input shape: (batch_size, seq_length, hidden_dim)
+        self.multi_head_attention = MultiheadAttention(hidden_dim, num_heads, batch_first=True) # input shape: (batch_size, seq_length, hidden_dim)
         self.ln2 = nn.LayerNorm(hidden_dim)
         self.mlp = nn.Sequential(
             nn.Linear(hidden_dim, mlp_dim),
@@ -20,7 +71,8 @@ class VisionTransformerBlock(nn.Module):
         # attn_output, attn_output_weights = self.multi_head_attention(out, out, out)
         # out = x + self.multi_head_attention(out, out, out)[0]
         out = self.ln1(x)
-        out, _ = x + self.multi_head_attention(out, out, out)
+        out, _ = self.multi_head_attention(out, out, out)
+        out = x + out
         out = out + self.mlp(self.ln2(out))
 
         return out
@@ -58,3 +110,7 @@ class VisionTransformer(nn.Module):
         x = self.clf(x)
 
         return x
+
+
+# model = VisionTransformer(image_size=(32, 32), hidden_dim=384, num_heads=6, mlp_dim=64*4, patch_size=4, depth=8, num_classes=10)
+# print(count_parameters(model))
